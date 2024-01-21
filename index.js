8456507cea8423ff692ffa923b3fbc2e4864ab03
vkahara <valtterikahara@gmail.com>
2024-01-21 14:47:56 +0200

    exercise 3.4

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ app.get('/api/persons/:id', (request, response) => {
       }
 })
 
+// delete a single phonebook entry 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
