7ca054d98c84e78f2094c92972f45e0cf58448fb
vkahara <valtterikahara@gmail.com>
2024-01-21 15:42:47 +0200

    exercise 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -59,6 +61,36 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+
+// generate new id that is 1 larger
+//than the previous largest id
+const generateId = () => {
+    const randomId = Math.floor(Math.random() * 10000000)
+    return randomId
+  }
+
+// add new phonebook entry. 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    // check if information missing
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'person information missing'
+        })
+    }
+    
+   console.log("body",request.body)
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
