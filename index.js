c39542165da78925f903e16e1a900f10a672670f
vkahara <valtterikahara@gmail.com>
2024-01-24 00:16:39 +0200

    exercise 3.19

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
   })
 
 // add new phonebook entry. 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     const person = new Person({
@@ -67,7 +67,7 @@ app.post('/api/persons', (request, response) => {
     })
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
 })
 
 //update person number
@@ -91,7 +91,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    }
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+      }
   
     next(error)
   }
