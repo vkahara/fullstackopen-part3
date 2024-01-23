ae5b1c79fe0a272bb53cb04c70da9a8f1cb1200b
vkahara <valtterikahara@gmail.com>
2024-01-23 12:15:41 +0200

    exercise 3.13

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
+// define token for morgan in order to log request content
 morgan.token('data', function (req, res) {
      return(
         JSON.stringify(req.body)
@@ -13,30 +16,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data' ))
 app.use(express.static('dist'))
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 
 // display info page with info on how may persons
 // the phonebook has and date
@@ -51,7 +30,9 @@ app.get('/info', (request, response) => {
 
 // get all data on persons
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 // functionality for displaying the information for a single phonebook entry. 
@@ -115,7 +96,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
