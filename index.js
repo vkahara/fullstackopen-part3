30998670e49e6c28ab892fed9019a2c662769b22
vkahara <valtterikahara@gmail.com>
2024-01-24 12:51:52 +0200

    exercise 3.21 and fix to 3.17

--- index.js
+++ index.js
@@ -72,14 +72,10 @@ app.post('/api/persons', (request, response, next) => {
 
 //update person number
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
+    
+    const { name, number } = request.body;
     Person.findByIdAndUpdate(request.params.id,
-        {name, number },
+        { name, number },
         { new: true, runValidators: true, context: 'query'})
         .then(updatedPerson => {
             response.json(updatedPerson)
